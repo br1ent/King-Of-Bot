fb42235b7a00b1a8e71544b3a3321a62ae205283
石铭轩 <530220399@qq.com>
2026-04-10 19:44:42 +0800

    websocket实现jwt验证

---
+++ web/src/store/pk.js
@@ -0,0 +1,29 @@
+﻿export default {
+    state: {
+        socket: null,
+        status: "matching", // matching:表示匹配中, playing:表示正在游戏
+        opponent_username: "",
+        opponent_photo: "",
+        opponent_rating: "",
+    },
+    getters: {
+    },
+    mutations: {
+        updateSocket(state, socket) {
+            state.socket = socket;
+        },
+        updateOpponentInfo(state, opponent) {
+            state.opponent_username = opponent.username;
+            state.opponent_photo = opponent.photo;
+            state.opponent_rating = opponent.rating;
+        },
+        updateStatus(state, status) {
+            state.status = status;
+        }
+    },
+    actions: {
+
+    },
+    modules: {
+    }
+}
--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -5,9 +5,37 @@
 </template>
 
 <script setup>
+import { onMounted, onUnmounted } from 'vue';
 import PlayGround from '../../components/PlayGround.vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+let socket = null;
+const socketUrl = `ws://127.0.0.1:3000/websocket/${store.state.user.token}`;
+onMounted(() => {
+    socket = new WebSocket(socketUrl);
+
+    socket.onopen = () => {
+        console.log("WebSocket连接已打开");
+    };
+
+    socket.onclose = () => {
+        console.log("WebSocket连接已关闭");
+    };
+
+    socket.onmessage = msg => {
+        const data = JSON.parse(msg.data);
+        console.log("收到消息:", data);
+    }
+});
+
+onUnmounted(() => {
+    if (socket) {
+        socket.close();
+    }
+});
+
 </script>
 
 <style scoped>
-
 </style>
